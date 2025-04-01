933ed0f9b11b2562c6cee659bf088f384d548eaf
skyfourhmmm <nguyenthientu413@gmail.com>
2025-04-01 14:48:03 +0700

    add: register and login

---
+++ src/assets/fakedata.js
@@ -0,0 +1,16 @@
+export const user = [
+  {
+    id: 1,
+    usename: "albert45",
+    password: "123456",
+    email: "admin@gmail.com",
+    role: "admin",
+  },
+  {
+    id: 2,
+    usename: "albert46",
+    password: "123456",
+    email: "customer@gmail.com",
+    role: "customer",
+  },
+];
